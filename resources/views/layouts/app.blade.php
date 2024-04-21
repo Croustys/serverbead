20b9cd08ee67ded80bc8804656fb1bb9b78b02e7
croustys <akos.barabas2002@gmail.com>
2024-04-21 20:07:12 +0200

    nav bar design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,25 +44,14 @@
                             <a href="{{ route('login') }}" class="text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium">Login</a>
                             <a href="{{ route('register') }}" class="text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium">Register</a>
                             @else
-                            <div class="ml-3 relative">
-                                <div>
-                                    <button type="button" class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu" aria-expanded="false" aria-haspopup="true">
-                                        <span class="sr-only">Open user menu</span>
-                                        <span class="inline-block h-8 w-8 rounded-full overflow-hidden bg-gray-100">
-                                        </span>
-                                    </button>
-                                </div>
-                                <div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
-                                    <div class="py-1" role="none">
-                                        <a href="{{ route('logout') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
-                                    </div>
-                                </div>
-                            </div>
+                            <a href="{{ route('logout') }}" class="text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium" role="menuitem" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
                             @endguest
                         </div>
                     </div>
-                    <div class="-mr-2 flex md:hidden">
-                        <!-- Mobile menu button -->
+                    <div class="mr-2 flex md:hidden">
                         <button type="button" class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
                             <span class="sr-only">Open main menu</span>
                             <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
@@ -90,7 +79,10 @@
                     <a href="{{ route('login') }}" class="text-white hover:bg-gray-700 block px-3 py-2 rounded-md text-base font-medium">Login</a>
                     <a href="{{ route('register') }}" class="text-white hover:bg-gray-700 block px-3 py-2 rounded-md text-base font-medium">Register</a>
                     @else
-                    <a href="{{ route('logout') }}" class="text-white hover:bg-gray-700 block px-3 py-2 rounded-md text-base font-medium" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                    <a href="{{ route('logout') }}" class="text-white hover:bg-gray-700 block px-3 rounded-md text-base font-medium" onclick="document.getElementById('logout-form').submit();">Logout</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
                     @endguest
                 </div>
             </div>
