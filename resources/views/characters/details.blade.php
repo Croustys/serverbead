2edbb840f3283c9d2af3baf52ddaa5acd89ec7ec
croustys <akos.barabas2002@gmail.com>
2024-04-21 21:33:53 +0200

    version bump

--- resources/views/characters/details.blade.php
+++ resources/views/characters/details.blade.php
@@ -19,30 +19,38 @@
                     @csrf
                     <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300">Új meccs</button>
                 </form>
+                <p><a href="{{ route('characters.edit', $character->id) }}" class="text-blue-500 hover:underline">Módosítás</a></p>
+                <form method="POST" action="{{ route('characters.destroy', $character->id) }}" onsubmit="return confirm('Are you sure you want to delete this character?')">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 focus:outline-none focus:ring focus:ring-red-300 mt-4">Karakter törlése</button>
+                </form>
                 <h3 class="text-lg font-semibold mb-2">Mérkőzések</h3>
                 <div class="divide-y divide-gray-200">
                     @if($character->contests->isNotEmpty())
                     @foreach($character->contests as $contest)
-                    <div class="py-4">
-                        <p class="text-sm text-gray-600 mb-2">{{ $contest->place->name }}</p>
-                        <div class="flex items-center space-x-2">
-                            @foreach($contest->characters as $char)
-                            <div class="flex-1">
-                                <p class="text-sm text-gray-600">
-                                    @if($contest->win)
-                                    <span class="text-green-500">Győztes: </span>
-                                    @endif
-                                    @if($char->id == $character->id)
-                                    <span class="text-green-500">Hős: </span>
-                                    @else
-                                    <span class="text-red-500">Ellenség: </span>
-                                    @endif
-                                    {{ $char->name }}
-                                </p>
+                    <a href="{{ route('contests.show', $contest) }}" class="flex-1 block cursor-pointer">
+                        <div class="py-4">
+                            <p class="text-sm text-gray-600 mb-2">{{ $contest->place->name }}</p>
+                            <div class="flex items-center space-x-2">
+                                @foreach($contest->characters as $char)
+                                <div class="flex-1">
+                                    <p class="text-sm text-gray-600">
+                                        @if($contest->win)
+                                        <span class="text-green-500">Győztes: </span>
+                                        @endif
+                                        @if($char->id == $character->id)
+                                        <span class="text-green-500">Hős: </span>
+                                        @else
+                                        <span class="text-red-500">Ellenség: </span>
+                                        @endif
+                                        {{ $char->name }}
+                                    </p>
+                                </div>
+                                @endforeach
                             </div>
-                            @endforeach
                         </div>
-                    </div>
+                    </a>
                     @endforeach
                     @else
                     <p>Nincsenek mérkőzések ehhez a karakterhez.</p>
--- resources/views/places/index.blade.php
+++ resources/views/places/index.blade.php
@@ -19,7 +19,11 @@
         <tr class="hover:bg-gray-100">
           <td class="px-4 py-2 border">{{ $place->name }}</td>
           <td class="px-4 py-2 border">
+            @if(Str::startsWith($place->image, 'http'))
             <img src="{{ $place->image }}" alt="Place Image" class="h-16">
+            @else
+            <img src="{{ asset('storage/places/' . $place->image) }}" alt="Place Image" class="h-16">
+            @endif
           </td>
           <td class="px-4 py-2 border">
             <form method="POST" action="{{ route('places.destroy', $place->id) }}" onsubmit="return confirm('Are you sure you want to delete this place?')">
@@ -27,6 +31,7 @@
               @method('DELETE')
               <button type="submit" class="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 focus:outline-none focus:ring focus:ring-red-300 mt-4">Helyszín törlése</button>
             </form>
+            <a href="{{ route('places.edit', $place->id) }}" class="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 focus:outline-none focus:ring focus:ring-green-300 mt-2 inline-block">Módosítás</a>
           </td>
         </tr>
         @endforeach
