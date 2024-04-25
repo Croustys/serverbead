4f5b20ba8ee5133ef2107eb7f88b5b8030ef39dd
croustys <akos.barabas2002@gmail.com>
2024-04-25 15:04:51 +0200

    auth on router

--- app/Routers/CharacterRouter.php
+++ app/Routers/CharacterRouter.php
@@ -12,14 +12,14 @@ class CharacterRouter
   {
     Route::prefix('characters')->group(function () {
       Route::get('/', [CharacterController::class, 'index'])->middleware(['auth', 'verified'])->name('characters.index');
-      Route::get('/create', [CharacterController::class, 'create'])->middleware(['auth', 'verified'])->name('characters.create');
       Route::post('/', [CharacterController::class, 'store'])->middleware(['auth', 'verified'])->name('characters.store');
+      Route::get('/create', [CharacterController::class, 'create'])->middleware(['auth', 'verified'])->name('characters.create');
       Route::get('/{character}/edit', [CharacterController::class, 'edit'])->middleware(['auth', 'verified'])->name('characters.edit');
       Route::get('/{id}', [CharacterController::class, 'details'])->middleware(['auth', 'verified'])->name('characters.details');
-      Route::put('/{character}', [CharacterController::class, 'update'])->name('characters.update');
-      Route::delete('/{character}', [CharacterController::class, 'destroy'])->name('characters.destroy');
+      Route::put('/{character}', [CharacterController::class, 'update'])->middleware(['auth', 'verified'])->name('characters.update');
+      Route::delete('/{character}', [CharacterController::class, 'destroy'])->middleware(['auth', 'verified'])->name('characters.destroy');
       Route::post('/{character}/matches', [ContestController::class, 'create'])->middleware(['auth', 'verified'])->name('matches.create');
-      Route::post('/{character}/start-match', [CharacterController::class, 'startMatch'])->name('characters.start-match');
+      Route::post('/{character}/start-match', [CharacterController::class, 'startMatch'])->middleware(['auth', 'verified'])->name('characters.start-match');
     });
   }
 }
